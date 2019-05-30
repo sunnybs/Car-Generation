6f7b6a8cd44e5abee0e267992849c2e57d82d3dc
sunny_bs <47714578+sunnybs@users.noreply.github.com>
2019-05-30 15:03:33 +0500

    transmission rect

--- Assets/Utils/Blueprints/SimpleBlueprint.cs
+++ Assets/Utils/Blueprints/SimpleBlueprint.cs
@@ -16,19 +16,10 @@ namespace Assets.Utils.Blueprints
 
         public override void StickTransmissions(List<GameObject> transmissions, CarMesh carMesh, float yLevel)
         {
-            var xOffset = 0f; //отступ для сдвига деталей по ширине
-            var zOffset = 0f; //отступ для сдвига деталей по длине
-            foreach (var transmission in transmissions)
-            {
-                var form = transmission.GetComponent<Detail>();
-
-                transmission.transform.Translate(xOffset, yLevel, zOffset);
-                carMesh.AddMesh(transmission.transform.position,
-                    new Vector3(form.MaxWidth, form.MaxHeight, form.MaxLength), DetailType.Transmission);
-                zOffset += form.MaxLength;
-            }
+            CarBuilder.TransformTransmissionsInRect(transmissions, carMesh, yLevel, transmissions.Count,1);
         }
 
+   
         public override void StickBody(GameObject body, CarMesh carMesh)
         {
             CarBuilder.TransformBody(body, carMesh, 5);
